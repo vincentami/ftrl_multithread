2b6f8298dea7a6e5a12f65badef3f8a3349460c5
lilifeng [李利锋] <lilifeng@xiaomi.com>
2017-04-26 19:02:52 +0800

    adjust delim
    Signed-off-by: lilifeng [李利锋] <lilifeng@xiaomi.com>

--- src/ftrl/ftrl.cpp
+++ src/ftrl/ftrl.cpp
@@ -265,8 +265,8 @@ double FTRL::predict(const std::vector<pair<std::string, double> >& fea) {
     delim = 0;
     i = 0;
     cout << ss.str() << endl;
-    cout << "getLine:" << std::getline(ss, item, delim) << endl;
-    while (std::getline(ss, item, delim)){
+    cout << "getLine:" << (!std::getline(ss, item)) << endl;
+    while (std::getline(ss, item)){
         i++;
         {
             splitRes.push_back(item);
