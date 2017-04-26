c456e5bfb42891c27431ce24d1ccc5b5977ba29c
lilifeng [李利锋] <lilifeng@xiaomi.com>
2017-04-26 17:53:18 +0800

    add fix
    Signed-off-by: lilifeng [李利锋] <lilifeng@xiaomi.com>

--- src/ftrl/ftrl.cpp
+++ src/ftrl/ftrl.cpp
@@ -235,7 +235,11 @@ double FTRL::predict(const std::vector<pair<std::string, double> >& fea) {
     } 
 
     if (splitRes.size() <= 14){
-        cout << "splitRes.size()" << splitRes.size() << endl;
+        if(splitRes.size() == 14){
+            cout << "splitRes.size:" << splitRes[14] << endl;
+        }else{
+            cout << "splitRes.size()" << splitRes.size() << endl;
+        }
         return false;
     }
 
